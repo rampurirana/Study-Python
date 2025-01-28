ce67c6e84c554d035dd82a53c35e562772a93038
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-28 19:33:48 +0530

    Print Statements

--- Day02.py
+++ Day02.py
@@ -1,16 +1,36 @@
-# This is a sinle line comment
+# A. Comments in Python
 
+# 1. This is a sinle line comment
 print("This is single line comment.")
 
 """
-This is a
+2. This is a
 multiple line
 comment.
 """
 print("This is a multiple line comment.")
 
-# Escape sequences character
+# B. Escape sequences character
 
 print("My name is Rampravesh Rana\nMy father name is Baleshwar Rana")
-
 print("Rampravesh said, \"Hello\"")
+
+# C. Print statements
+
+# 1. Simple print :-
+print("Hello, world!")
+
+# 2. Multiple items print :-
+print("Python", "is", "fun")
+
+# 3. Using seperator ( sep ) :-
+print("Python", "is", "fun", sep='-')
+
+# 4. Using ending character ( end ) :-
+print("Rampravesh Rana", end='-')
+print("Dapok")
+
+# 5. formatted printing :-
+name = "Rampravesh Rana"
+age = 20
+print(f"My name is {name} and my age is {age}.")
