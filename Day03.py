d9c6c331a7413fa36306e50f54af32fe77eba99b
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-30 18:57:46 +0530

    Non Primitive data types

--- Day03.py
+++ Day03.py
@@ -11,13 +11,24 @@ print(x, pie, name, is_student)
 
 # A. Primitive Data Types : That store simple values
 
-integer = 20
-floating = 3.14
-fullName = "Rampravesh Rana"
-boolean = False
-noneType = None
+integer = 20 #int
+floating = 3.14 #float
+fullName = "Rampravesh Rana" #string
+boolean = False #boolean
+noneType = None #NoneType
 print(type(integer))
 print(type(floating))
 print(type(fullName))
 print(type(boolean))
 print(type(noneType))
+
+# B. Non Primitive data types
+
+fruits = ["apple", "banana", "mango"] #list
+numbers = (1, 2, 3) #tuple
+unique_numbers = {1, 2, 3, 4, 5, 1} #set
+person = {"name": "Rampravesh Rana", "age": 20} #dic
+print(type(fruits))
+print(type(numbers))
+print(type(unique_numbers))
+print(type(person))
