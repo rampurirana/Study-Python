4fa0a35888f0c54ce7ec51dcb2dbfb08ac19a680
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-30 18:50:10 +0530

    Primitive data types

--- Day03.py
+++ Day03.py
@@ -6,3 +6,18 @@ name = "Rampravesh Rana"
 is_student = True
 
 print(x, pie, name, is_student)
+
+# Data Types
+
+# A. Primitive Data Types : That store simple values
+
+integer = 20
+floating = 3.14
+fullName = "Rampravesh Rana"
+boolean = False
+noneType = None
+print(type(integer))
+print(type(floating))
+print(type(fullName))
+print(type(boolean))
+print(type(noneType))
