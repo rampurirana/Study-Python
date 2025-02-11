d9c7306b7441ba085a7cb3e515881b47712243c2
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 19:01:15 +0530

    Taking user input

---
+++ Day 04/UserInput.py
@@ -0,0 +1,16 @@
+# Taking user input using the input() function. By default, the inpur() function takes the input as string.
+
+# 1. Basic input (String) :
+name = input("Enter your name : ")
+print("Name :", name)
+
+# 2. Integer input :
+age = int(input("Enter your age : "))
+print("Age :", age)
+
+# 3. Float input :
+height = float(input("Enter your height : "))
+print("Height :", height)
+
+# Formatted Print
+print(f"My name is {name} and i am {age} years old. My height is {height}ft.")
