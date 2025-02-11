0205389cc7d791250b13be3c2e09e41ceebe39a5
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 19:33:49 +0530

    Creating String

---
+++ Day 04/StringCreate.py
@@ -0,0 +1,10 @@
+name = 'Rampravesh'
+print("Name :", name)
+
+fullName = "Rampravesh Rana"
+print("Full Name :", fullName)
+
+multiline = """My name is
+Rampravesh Rana.
+I am 20 years old."""
+print(multiline)
