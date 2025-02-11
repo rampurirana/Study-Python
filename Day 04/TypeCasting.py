595249f044a1c6124dd80b34ec1f5b7e8b5b4d7a
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 18:28:13 +0530

    Type Casting

---
+++ Day 04/TypeCasting.py
@@ -0,0 +1,14 @@
+# Type Casting is the process of converting one data type into another
+
+# Types of type casting
+
+# 1. Explicit conversion (Type Casting) : Manually convert one data type to another data type using bult in functions like int(), float(), str()
+num_float = 5.8
+num_int = int(num_float)
+print(num_int)
+
+# 2. Imlicit conversion (Type Casting) : Python automatically convert one data type into another data type during an operation without any user intervetion
+number_int = 10
+number_float = 4.5
+result = number_int + number_float
+print(result)
