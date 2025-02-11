175baa2e52e6594f4bddddf93ec7820e18b219f3
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-11 19:36:38 +0530

    Creating String

--- Day 04/StringCreate.py
+++ Day 04/StringCreate.py
@@ -1,9 +1,12 @@
+# Single Quotes
 name = 'Rampravesh'
 print("Name :", name)
 
+# Double Quotes
 fullName = "Rampravesh Rana"
 print("Full Name :", fullName)
 
+# Triple Quotes for multiline String
 multiline = """My name is
 Rampravesh Rana.
 I am 20 years old."""
