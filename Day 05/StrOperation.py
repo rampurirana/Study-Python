32b5dbb915974c98cfe1d7dee10405207a268495
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-17 20:29:32 +0530

    String Operation in Python

---
+++ Day 05/StrOperation.py
@@ -0,0 +1,15 @@
+# String Operation
+
+# 1. Concatenation : Joining string with +
+firstName = "Rampravesh"
+lastName = "Rana"
+fullName = firstName + " " + lastName
+print(fullName)
+
+# 2. Repetition : Repeat string with *
+repeatName = "Yes" * 3
+print(repeatName)
+
+# 3. Length of string : len() function
+nickName = "Rampuri Rana"
+print(len(nickName))
