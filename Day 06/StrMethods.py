f0f27bac6b1bf47e6a940ea83f93cfb77355568e
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-18 21:04:32 +0530

    Case Conversion Methods

---
+++ Day 06/StrMethods.py
@@ -0,0 +1,9 @@
+# String Methods : Python provides a variety of built-in string mehtods that help manipulating and process strings efficiently
+
+# 1. Case Conversion :
+name = "raMpravesH raNa"
+print(name.upper()) #converts all character to uppercase
+print(name.lower()) #converts all character to lowerrcase
+print(name.title()) #capitalizes the first letter of each word
+print(name.capitalize()) #capitalizes the first letter of the strings
+print(name.swapcase()) #swaps uppercase to lowercase
