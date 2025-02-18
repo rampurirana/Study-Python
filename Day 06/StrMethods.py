a32d2bf214352c819d9e812ef1997de445ebb7ed
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-18 21:17:19 +0530

    Checking string content

--- Day 06/StrMethods.py
+++ Day 06/StrMethods.py
@@ -7,3 +7,13 @@ print(name.lower()) #converts all character to lowerrcase
 print(name.title()) #capitalizes the first letter of each word
 print(name.capitalize()) #capitalizes the first letter of the strings
 print(name.swapcase()) #swaps uppercase to lowercase
+
+# 2. Checking String Content :
+check = "My age is 20"
+print(check.isalpha()) #check if all character are letters
+print(check.isdigit()) #check if all character are digits
+print(check.isalnum()) #check if all character are letters or numbers
+print(check.isspace()) #check if string contains only whitspaces
+print(check.isupper()) #check if all character are uppercase
+print(check.islower()) #check if all character are lowercase
+print(check.istitle()) #check if string follows title case
