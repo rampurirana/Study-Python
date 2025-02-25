35a6d628acd9ce60d58dd871f583386f33996864
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-25 20:48:31 +0530

    Simple if-else conditional statement

---
+++ Day 07/if-else.py
@@ -0,0 +1,9 @@
+# if-else : if-else conditional statement is used to execute different blocks of code based on a condition
+
+# simple if-else conditional statement
+age = int(input("Please enter your age : "))
+
+if age >= 18:
+    print("You are eligible for driving")
+else:
+    print("You are not eligible for driving")
