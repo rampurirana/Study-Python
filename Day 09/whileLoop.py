8627ed209624720f6c0602e4c19309a15f2dc7e2
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-17 21:11:36 +0530

    while loop

---
+++ Day 09/whileLoop.py
@@ -0,0 +1,6 @@
+# while loop : Repeats a block of code while a condition is true
+
+x = 0 # intilized the value
+while x < 5: # condition
+    print(x) # print
+    x = x + 1 # increment or decrement
