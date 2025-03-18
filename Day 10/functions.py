644624117cd47bc92e7c4162feb6e144370bb8e7
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 21:15:52 +0530

    Built in functions in pyhton

--- Day 10/functions.py
+++ Day 10/functions.py
@@ -5,3 +5,8 @@ def greet(name): # greet is the name of functions, name is the parameter
     print(f"Hello, {name}") # functions body
 
 greet("Rampravesh Rana") # functions call
+
+# 1. Built-in functions : These are functions that come pre defined in python and can be used directly without defining them
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers)) #5
+print(sum(numbers)) #15
