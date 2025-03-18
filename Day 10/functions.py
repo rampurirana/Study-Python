4cec18ce2a0c6a3ccd60757f3cd428404cd75be0
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 21:24:00 +0530

    User defined function explanation

--- Day 10/functions.py
+++ Day 10/functions.py
@@ -10,3 +10,16 @@ greet("Rampravesh Rana") # functions call
 numbers = [1, 2, 3, 4, 5]
 print(len(numbers)) #5
 print(sum(numbers)) #15
+
+# 2. User defined functions : These are the functions created by user to perform specific tasks. Define using the def keyword
+def isdivide(a, b):
+    if(a % b == 0):
+        print("Divisible by two")
+    else:
+        print("Not divisible by two")
+
+a = 25
+b = 8
+isdivide(a, b) # not divisible
+
+isdivide(10, 5) # divisible
