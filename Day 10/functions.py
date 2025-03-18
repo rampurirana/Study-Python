b6597c127168d9c11e7a14c2a7b40a514c423d78
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-18 21:12:12 +0530

    Functions explanation

---
+++ Day 10/functions.py
@@ -0,0 +1,7 @@
+# Functions : A functions is a block of code that performs a specific task whenever it is called.
+# functions is defined using the def keyword
+
+def greet(name): # greet is the name of functions, name is the parameter
+    print(f"Hello, {name}") # functions body
+
+greet("Rampravesh Rana") # functions call
