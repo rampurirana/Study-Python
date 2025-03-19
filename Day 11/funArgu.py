6754b0c5a89182341598add8fe654f6376c5947a
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-19 20:43:21 +0530

    Keyword Arguments

--- Day 11/funArgu.py
+++ Day 11/funArgu.py
@@ -4,6 +4,11 @@
 def greet(name, age = 20):
     print("Name :", name)
     print("Age :", age)
-
 greet("Rampravesh Rana") # uses default age = 20
 greet("Ashish Rana", 25) # overrides default age
+
+# 2. Keeyword Arguments : Keyword arguments that use parameter names to assign the values rather than order
+def details(name, age):
+    print("Name :",name)
+    print("Age :",age)
+details(age = 15, name = "Sagar") # order does not matter
