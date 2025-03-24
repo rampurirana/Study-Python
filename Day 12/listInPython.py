af56b0b8af9424277532e172b67d832252da4538
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-24 20:40:26 +0530

    List in python

---
+++ Day 12/listInPython.py
@@ -0,0 +1,7 @@
+# A list is a mutable, ordered of collection of items. It can hold elements of different data types.
+
+# Creating List : Create list using square brackets[] or the list() constructor
+
+# list with different data types
+myList = [1, 2, 3, "Rampravesh", 6.5, True]
+print(myList)
