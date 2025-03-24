88dc03f1157fc01871f1b5b4b040cd6ad3b78e12
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-03-24 20:46:38 +0530

    Accessing elements from lists

--- Day 12/listInPython.py
+++ Day 12/listInPython.py
@@ -5,3 +5,8 @@
 # list with different data types
 myList = [1, 2, 3, "Rampravesh", 6.5, True]
 print(myList)
+
+# Accessing elements : Access elements in list using indexing (Starting from 0) or negative indexing
+fruits = ["apple", "Banana", "Mango", "Cherry"]
+print(fruits[0]) # apple
+print(fruits[-2]) # subtract from len of list = 4-2 = Mango
